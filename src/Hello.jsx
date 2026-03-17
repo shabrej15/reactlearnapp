5c0ce4ea2dac982a64f9c641b247a450591806b8
Shabrej Ahmad <shabrej.ahmad@gmail.com>
2026-03-17 16:34:39 +0530

    inline styles

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,6 +1,20 @@
 const Hello = ()=>{
     const name = "Shabrej Ahmad";
-    return <h2 className="test">Hello {name} component</h2>
-}
+    const paragraphstyles = {
+        color:"green",
+        fontSize:30,
+        backgroundColor:"pink"
+    }
+    
+    return (
+        <div>
+            <h2 className="test">Hello {name} component</h2>
+<p style={paragraphstyles}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex doloremque, dolores laboriosam officia tempora eos ea maiores est tenetur minima fugiat voluptates nostrum facilis odio minus nemo, provident sapiente mollitia!</p>
+<p style={{color:"blue",fontSize:30,backgroundColor:"gray"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex doloremque, dolores laboriosam officia tempora eos ea maiores est tenetur minima fugiat voluptates nostrum facilis odio minus nemo, provident sapiente mollitia!</p>
+        </div>
+        
+    );
+    
+};
 
 export default Hello;
