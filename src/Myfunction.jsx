5f192961c458a62a42e41aea0a875125429bb9d7
Shabrej Ahmad <shabrej.ahmad@gmail.com>
2026-03-17 17:49:10 +0530

    Function completed

--- src/Myfunction.jsx
+++ src/Myfunction.jsx
@@ -1,10 +1,23 @@
 const Myfunction = ()=>{
 
-    const name = ()=>{
-        return "Shabrej Ahmad";
+    // const name = ()=>{
+    //     return "Shabrej Ahmad";
+    // }
+
+    const getName = (names)=>{
+        return names;
     }
+
+    const name = "Shabrej";
+    const name1 = "Ahmad";
+
     return (
-        <h1>Function Component {name()}</h1>
+        <div>
+<h1 className="heading">Function Component {getName(name)}</h1>
+        <h2 className="test">Function Component {getName(name1)}</h2>
+        
+        </div>
+        
     )
 }
 
