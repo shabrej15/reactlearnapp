9cb84dc14214242ac944e6b0a36cfe6f5c5c3788
Shabrej Ahmad <shabrej.ahmad@gmail.com>
2026-03-17 16:47:26 +0530

    external styles

--- src/Bye.jsx
+++ src/Bye.jsx
@@ -1,5 +1,10 @@
 const Bye= ()=>{
-    return <h1>Bye Component</h1>
+    return (
+<div>
+    <h1 className="heading">Bye Component</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates quas quasi harum dignissimos nihil id ullam, repellat voluptas. Doloribus quas qui ea dignissimos eius exercitationem delectus rem magnam dolores nemo.</p>
+</div>
+    ) 
 }
 
 export default Bye;
