bbb0a5fe64ee247f900af023d14fe524d596b51e
Shabrej Ahmad <shabrej.ahmad@gmail.com>
2026-03-25 13:48:57 +0530

    state management toggle liked

--- src/StateManagement.jsx
+++ src/StateManagement.jsx
@@ -2,6 +2,8 @@ import {useState} from 'react'
 
 function StateManagement() {
     const [count, setCount] = useState(0)
+    const [isVisible, setIsVisible] = useState(false)
+    const [isLike, setIsLike] = useState(false)
   return (
     <div>
         <h1>StateManagement</h1>
@@ -9,7 +11,15 @@ function StateManagement() {
        
         <button onClick={()=>setCount(count+1)}>Count</button><br /><br />
 
-        <button onClick={()=>setCount(count-1)}>Count Decrease</button>
+        <button onClick={()=>setCount(count-1)}>Count Decrease</button><br /><br />
+
+        <button onClick={()=>setIsVisible(!isVisible)}>{isVisible ? 'Hide' : 'Show'} Text</button>
+
+        {isVisible && <p>This is secret text</p>}<br /><br />
+
+        <button onClick={()=>setIsLike(!isLike)}>{isLike ? '❤️ Liked' : '💙 Like'}</button>
+
+
     </div>
   )
 }
