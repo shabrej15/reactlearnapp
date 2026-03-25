10b55266abadb889eb25feea8059f3e525adb197
Shabrej Ahmad <shabrej.ahmad@gmail.com>
2026-03-25 15:53:25 +0530

    state management for array

--- src/StateManagement.jsx
+++ src/StateManagement.jsx
@@ -4,6 +4,8 @@ function StateManagement() {
     const [count, setCount] = useState(0)
     const [isVisible, setIsVisible] = useState(false)
     const [isLike, setIsLike] = useState(false)
+    const [name, setName] = useState("Guest")
+    const [age, setAge] = useState(18)
   return (
     <div>
         <h1>StateManagement</h1>
@@ -17,7 +19,12 @@ function StateManagement() {
 
         {isVisible && <p>This is secret text</p>}<br /><br />
 
-        <button onClick={()=>setIsLike(!isLike)}>{isLike ? '❤️ Liked' : '💙 Like'}</button>
+        <button onClick={()=>setIsLike(!isLike)}>{isLike ? '❤️ Liked' : '💙 Like'}</button><br /><br />
+
+    <h1>Name is : {name}</h1>
+        <button onClick={()=>setName("Shabrej Ahmad")}>Change Name</button>
+<h1>Age is : {age}</h1>
+        <button onClick={()=>setAge(age+1)}>Change Age</button>
 
 
     </div>
